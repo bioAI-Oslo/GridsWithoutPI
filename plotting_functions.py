28b5196f5eae5879594f2692359699c054638250
vemundss <vemund@live.com>
2024-02-15 10:23:21 +0100

    pattern formation analysis

--- plotting_functions.py
+++ plotting_functions.py
@@ -1,20 +1,22 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from mpl_toolkits.axes_grid1 import ImageGrid
 
-def multiimshow(zz, figsize=(1,1), normalize=True, add_colorbar=True, rect=(0,0,1,0.87), axes_pad=0.05, **kwargs):
+def multiimshow(zz, figsize=(1,1), normalize=True, add_colorbar=True, rect=(0,0,1,0.87), axes_pad=0.05, fig=None, **kwargs):
     # prepare figure
     ncols = int(np.ceil(np.sqrt(zz.shape[0])))
     nrows = int(round(np.sqrt(zz.shape[0])))
-    from mpl_toolkits.axes_grid1 import ImageGrid
-    fig = plt.figure(figsize=figsize)
+    fig = plt.figure(figsize=figsize) if fig is None else fig
     if add_colorbar and normalize:
         grid = ImageGrid(fig, rect=rect, nrows_ncols=(nrows, ncols), axes_pad=axes_pad, cbar_mode='single', cbar_location='right', cbar_pad=0.1, cbar_size='5%')
     else:
         grid = ImageGrid(fig, rect=rect, nrows_ncols=(nrows, ncols), axes_pad=axes_pad)
     vmin, vmax = (np.nanmin(zz), np.nanmax(zz)) if normalize else (None, None)
+    kwargs['vmin'] = vmin if 'vmin' not in kwargs else kwargs['vmin']
+    kwargs['vmax'] = vmax if 'vmax' not in kwargs else kwargs['vmax']
     # plot response maps using imshow
     for ax, data in zip(grid, zz):
-        im = ax.imshow(data, vmin=vmin, vmax=vmax, **kwargs)
+        im = ax.imshow(data, **kwargs)
     [ax.axis('off') for ax in grid]
     fig.colorbar(im, cax=grid.cbar_axes[0]) if (normalize and add_colorbar) else None
     return fig, grid.axes_all
