0edcafb90854fce47caeb5f339079a5546521d05
vemundss <vemund@live.com>
2024-02-15 11:26:23 +0100

    makes vmin vmax symmetric for divergent cmap

--- plotting_functions.py
+++ plotting_functions.py
@@ -2,23 +2,24 @@ import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.axes_grid1 import ImageGrid
 
-def multiimshow(zz, figsize=(1,1), normalize=True, add_colorbar=True, rect=(0,0,1,0.87), axes_pad=0.05, fig=None, **kwargs):
+def multiimshow(zz, figsize=(1,1), add_colorbar=True, rect=(0,0,1,0.87), axes_pad=0.05, fig=None, vrange='minmax', **kwargs):
     # prepare figure
     ncols = int(np.ceil(np.sqrt(zz.shape[0])))
     nrows = int(round(np.sqrt(zz.shape[0])))
     fig = plt.figure(figsize=figsize) if fig is None else fig
-    if add_colorbar and normalize:
+    if add_colorbar:
         grid = ImageGrid(fig, rect=rect, nrows_ncols=(nrows, ncols), axes_pad=axes_pad, cbar_mode='single', cbar_location='right', cbar_pad=0.1, cbar_size='5%')
-    else:
-        grid = ImageGrid(fig, rect=rect, nrows_ncols=(nrows, ncols), axes_pad=axes_pad)
-    vmin, vmax = (np.nanmin(zz), np.nanmax(zz)) if normalize else (None, None)
+    vmin, vmax = (np.nanmin(zz), np.nanmax(zz))
+    if vrange is 'symmetric':
+        vmax = max(abs(vmin), abs(vmax))
+        vmin = -vmax
     kwargs['vmin'] = vmin if 'vmin' not in kwargs else kwargs['vmin']
     kwargs['vmax'] = vmax if 'vmax' not in kwargs else kwargs['vmax']
     # plot response maps using imshow
     for ax, data in zip(grid, zz):
         im = ax.imshow(data, **kwargs)
     [ax.axis('off') for ax in grid]
-    fig.colorbar(im, cax=grid.cbar_axes[0]) if (normalize and add_colorbar) else None
+    fig.colorbar(im, cax=grid.cbar_axes[0]) if add_colorbar else None
     return fig, grid.axes_all
 
 
