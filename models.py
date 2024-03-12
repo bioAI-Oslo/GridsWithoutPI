23f3eb168f82518df9b074ed8dd9aa4a43be37c7
vemundss <vemund@live.com>
2024-03-12 09:58:30 +0100

    makes ratemap generation easier

--- models.py
+++ models.py
@@ -1,9 +1,10 @@
 import torch 
 import numpy as np
 import pickle
+import os
 
 class FFGC(torch.nn.Module):
-    def __init__(self, ng=256, alpha = 0.9, sigma = 1, norm = "l1"):
+    def __init__(self, ng=256, alpha = 0.88, sigma = 1, norm = "l1"):
         super().__init__()
         self.ng = ng
         self.alpha = alpha
@@ -21,6 +22,10 @@ class FFGC(torch.nn.Module):
         self.similarity_loss_history = []
         self.capacity_loss_history = []
         self.total_loss_history = []
+
+    @property
+    def device(self):
+        return next(self.parameters()).device
 
     def norm_relu(self, x):
         rx = self.relu(x)
@@ -70,13 +75,54 @@ class FFGC(torch.nn.Module):
         return loss
 
     def save(self, path=None):
-        path = f"./saved-models/{self.__class__.__name__}.pkl" if path is None else path
+        path = f"./saved-models/{self.__class__.__name__}_{len(self.total_loss_history)}.pkl" if path is None else path
+        device = self.device
+        self.to(torch.device("cpu"))
         with open(path, "wb") as f:
             pickle.dump(self, f)
+        self.to(device)
+
+    def get_model_list(self):
+        model_list = ['./saved-models/'+f for f in os.listdir("./saved-models/") if f.startswith(self.__class__.__name__)]
+        sorted_list = sorted(model_list, key = lambda x: int(x.split("_")[-1].split(".")[0]))
+        return sorted_list
 
     def load(self, path=None):
-        path = f"./saved-models/{self.__class__.__name__}.pkl" if path is None else path
+        if path is None:
+            model_list = self.get_model_list()
+            path = model_list[-1]
         return pickle.loads(open(path, "rb").read())
+
+    def forward_ratemaps(self, layer, output_unit=None, sort_idxs=None, rmin=-2*np.pi, rmax=2*np.pi, res=64):
+        # define domain
+        x = np.linspace(rmin, rmax, res)
+        y = np.linspace(rmin, rmax, res)
+        xx, yy = np.meshgrid(x, y)
+        r = np.stack((xx,yy), axis = -1).reshape(-1, 2) # (res*res, 2)
+        r = torch.tensor(r.astype('float32'), device = self.device)
+        # investigate codomain
+        if layer == 'full':
+            activity = self(r)
+        elif layer > -1:
+            activity = self.rg[:layer+1](r)
+        else:
+            # domain is the codomain
+            activity = r
+        activity = activity.detach().cpu().numpy()
+        # investigate output unit 
+        if output_unit is not None:
+            # layer might be "relu", so we get the weights from the previous 'layer'
+            try:
+                weight = self.rg[layer+1].weight.detach().cpu().numpy()[output_unit] # (ncells,)
+            except AttributeError:
+                weight = self.rg[layer].weight.detach().cpu().numpy()[output_unit] # (ncells,)
+        else:
+            weight = np.ones(activity.shape[-1])
+        activity = activity * weight # (res*res, ncells)
+        # sort by aggregate activity
+        sort_idxs = np.argsort(np.sum(activity, axis = 0))[::-1] if sort_idxs is None else sort_idxs
+        activity = activity[:,sort_idxs]
+        return activity.T.reshape(-1, res, res), sort_idxs
 
     
 class RNNGC(FFGC):
